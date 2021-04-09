236526cdb81c9837298e914bf69baaeeee90fb8a
Masen Furer <m_github@0x26.net>
2021-04-08 23:52:56 -0700

    api.js: endpoints for querying talkgroups and calls

--- src/api.js
+++ src/api.js
@@ -35,30 +35,69 @@ app.get('/pnwusers', async (req, res) => {
     res.send(result.rows);
 })
 
-app.get('/radio_id/:radio_id', async (req, res) => {
+app.get('/pnwtalkgroups', async (req, res) => {
     const client = await pool.connect();
-    const limit = res.query ? (req.query.limit ? parseInt(res.query.limit) : 100) : 100;
-    const result = await client.query(
-        "SELECT time, duration, source_peer, dest_group, calls.radio_id, callsign, name, state, country " +
-        "FROM calls " +
-        "JOIN users ON users.radio_id = calls.radio_id " +
-        "WHERE calls.radio_id = $1 " +
-        "ORDER BY time DESC " +
-        "LIMIT $2", [req.params.radio_id, limit],
-    )
+    const default_since = "2 day";
+    const since = req.query ? (req.query.since ? req.query.since : default_since) : default_since;
+    const result = await client.query(`
+        SELECT
+            MAX(time) AS last_heard,
+            COUNT(*) as n_calls,
+            talkgroup
+        FROM calls
+        WHERE time > NOW() - $1::interval 
+        GROUP BY talkgroup
+        ORDER BY last_heard DESC;
+    `, [since]);
     res.send(result.rows);
-
 })
 
-app.get('/callsign/:callsign', async (req, res) => {
+app.get('/calls_by_talkgroup/:talkgroup', async (req, res) => {
     const client = await pool.connect();
-    const result = await client.query(
-        "SELECT radio_id, callsign, name, city, state, country " +
-        "FROM users " +
-        "WHERE callsign = $1 ", [req.params.callsign.toUpperCase()],
-    )
+    const default_since = "2 day";
+    const since = req.query ? (req.query.since ? req.query.since : default_since) : default_since;
+    const result = await client.query(`
+        SELECT time, duration, source_peer, talkgroup, calls.radio_id
+        FROM calls
+        JOIN users ON users.radio_id = calls.radio_id
+        WHERE LOWER(talkgroup) = LOWER($1) AND time > NOW() - $2::interval
+        ORDER BY time DESC
+    `, [req.params.talkgroup, since]);
     res.send(result.rows);
+})
 
+app.get('/calls_by_id/:radio_id', async (req, res) => {
+    const client = await pool.connect();
+    const limit = res.query ? (req.query.limit ? parseInt(res.query.limit) : 100) : 100;
+    const result = await client.query(`
+        SELECT time, duration, source_peer, dest_group, calls.radio_id
+        FROM calls
+        JOIN users ON users.radio_id = calls.radio_id
+        WHERE calls.radio_id = $1
+        ORDER BY time DESC
+        LIMIT $2
+    `, [req.params.radio_id, limit]);
+    res.send(result.rows);
+})
+
+app.get('/user_by_callsign/:callsign', async (req, res) => {
+    const client = await pool.connect();
+    const result = await client.query(`
+        SELECT radio_id, callsign, name, city, state, country
+        FROM users
+        WHERE callsign = $1
+    `, [req.params.callsign.toUpperCase()]);
+    res.send(result.rows);
+})
+
+app.get('/user_by_id/:radio_id', async (req, res) => {
+    const client = await pool.connect();
+    const result = await client.query(`
+        SELECT radio_id, callsign, name, city, state, country
+        FROM users
+        WHERE radio_id = $1
+    `, [req.params.radio_id]);
+    res.send(result.rows);
 })
 
 app.listen(port, () => {
